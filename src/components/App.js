ed08cb1b6b3747f2027571a015cb026e0fe6c3fa
AlenaZavadskaya <helen.zavadskaya@yandex.ru>
2020-11-01 15:20:32 +0300

    create props.children in popups

--- src/components/App.js
+++ src/components/App.js
@@ -33,7 +33,38 @@ function App() {
 		setIsEditProfilePopupOpen();
 		setIsAddPlacePopupOpen();
 		setIsEditAvatarPopupOpen();
-		}
+	}
+	
+	const childrenProfilePopup =
+			<>
+			<label className="form__field"><input id="name-input" className="form__item form__item_name" type="text"
+								value="" name="full_name" placeholder="Имя" minLength="2" maxLength="40" required />
+							<span id="name-input-error" className="form__item-error"></span></label>
+						<label className="form__field form__field-info"><input id="about-input" className="form__item form__item_about"
+								type="text" value="" name="about" placeholder="О себе" minLength="2" maxLength="200"
+								required />
+					<span id="about-input-error" className="form__item-error"></span></label>
+				</>
+
+
+	const childrenPlacePopup = 
+			<>
+			<label className="form__field"><input id="title-input" className="form__item form__item_title" type="text" value=""
+								name="title" placeholder="Название" minLength="1" maxLength="30" required />
+							<span id="title-input-error" className="form__item-error"></span></label>
+						<label className="form__field form__field-info"><input id="url-input" className="form__item form__item_link"
+								type="url" value="" name="link" placeholder="Ссылка на картинку" required />
+							<span id="url-input-error" className="form__item-error"></span></label>
+				</>
+		
+
+	const childrenAvatarPopup =
+			<>
+			<label className="form__field form__field-info"><input id="url-input" className="form__item form__item_avatar form__item_link"
+								type="url" value="" name="link" placeholder="Ссылка на картинку" required />
+							<span id="url-input-error" className="form__item-error"></span></label>
+				</>
+
 	
 
   return (
@@ -49,7 +80,7 @@ function App() {
 				button="Сохранить"
 				isOpen={isEditProfilePopupOpen}
 				onSubmit=""
-				onClose={closeAllPopups}></PopupWithForm>
+				onClose={closeAllPopups}>{childrenProfilePopup}</PopupWithForm>
 			<PopupWithForm
 				name="popupPlace"
 				id="form-card"
@@ -57,7 +88,7 @@ function App() {
 				button="Создать"
 				isOpen={isAddPlacePopupOpen}
 				onSubmit=""
-				onClose={closeAllPopups}></PopupWithForm>
+				onClose={closeAllPopups}>{childrenPlacePopup}</PopupWithForm>
 			<PopupWithForm
 				name="popupAvatar"
 				id="form-avatar"
@@ -65,7 +96,7 @@ function App() {
 				button="Сохранить"
 				isOpen={isEditAvatarPopupOpen}
 				onSubmit=""
-				onClose={closeAllPopups}></PopupWithForm>
+				onClose={closeAllPopups}>{childrenAvatarPopup}</PopupWithForm>
 			<ImagePopup />
 			<PopupWithSubmit />
 		</>
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -10,13 +10,6 @@ function PopupWithForm(props) {
 					<h2 className="form__heading">{props.title}</h2>
 						<fieldset className="form__input-container">
 							{props.children}
-						{/* <label className="form__field"><input id={props.id} onChange={props.handleChange} className="form__item" type="text"
-								value="" name={props.name} placeholder={props.placeholder} minLength="2" maxLength="40" required />
-							<span id={props.id} className="form__item-error"></span></label>
-						<label className="form__field"><input id={props.id} onChange={props.handleChange} className="form__item"
-								type={props.type} value="" name={props.name} placeholder={props.placeholder} minLength="2" maxLength="200"
-								required />
-							<span id={props.id} className="form__item-error"></span></label> */}
 						<div className="form__handlers">
 							<button className="submit__button" type="submit" onSubmit={props.handleClick}>{props.button}</button>
 						</div>
